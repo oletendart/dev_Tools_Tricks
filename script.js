fa0fcf5f0b0d30dae87ba9b87634bd8137780cdb
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 16:22:10 +0100

    create const dogs contains 2 objects in the array + create function makeGreen target tag p + style and size

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const dogs = [
+  { name: "Snickers", age: 2 },
+  { name: "hugo", age: 8 }
+];
+
+function makeGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
+}
