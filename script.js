d9b89d3269c8c686df2c2f57cfb7ebedd30e9e45
Olivia Letendart <letendartolivia@gmail.com>
2020-01-22 10:02:42 +0100

    modification of console.group to console.groupCollapsed which directly reduces the group

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ console.clear();
 
 // Grouping together
 dogs.forEach(dog => {
-  console.group(`${dog.name}`);
+  console.groupCollapsed(`${dog.name}`);
   console.log(`This is ${dog.name}`);
   console.log(`${dog.name} is ${dog.age} years old`);
   console.log(`${dog.name} is ${dog.age * 7} dog years old`);
